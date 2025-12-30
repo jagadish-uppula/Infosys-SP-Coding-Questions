0586bdf8bb5253419385ee30a3f1756aed62e8ed
Uppula Jagadish <jagadishuppula@gmail.com>
2025-12-30 17:06:58 +0530

    Update SubsetSum.java

--- DynamicProgramming/SubsetSum.java
+++ DynamicProgramming/SubsetSum.java
@@ -4,6 +4,18 @@ public class SubsetSum {
     public static void main(String[] args) {
         int[] arr = { 3, 34, 4, 12, 5, 2 };
         int sum = 9;
+        boolean answer = subSetCheck(arr,sum);
+        System.out.println("Sum of Subset answer is: " + answer); 
+    }
+    static boolean subSetCheck(int[] arr, int sum){
+        boolean[] dp = new boolean[sum+1];
+        dp[0] = true;
 
+        for(int num : arr){
+            for(int j = sum; j >= num; j--){
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        return dp[sum]; 
     }
 }
